038daa258078bfeb21a983447aecc432318fc435
Henrique Almeida <henrique.almeida.dev@gmail.com.>
2025-07-01 11:25:00 -0300

    feat: add projection function.

--- src/transformer/pipeline/test_softmax.py
+++ src/transformer/pipeline/test_softmax.py
@@ -2,10 +2,10 @@ import numpy as np
 from softmax import softmax
 
 def main():
-    array = [10, 5, 1, 12]
-    probs = softmax(array)
+    logits = [10, 5, 1, 12]
+    probs = softmax(logits)
 
-    print("Array (pontuações):", ", ".join(str(x) for x in array))
+    print("Logits (pontuações):", ", ".join(str(x) for x in logits))
     print("Softmax (probabilidades): [", ", ".join(str(p) for p in probs), "]")
     print("Soma das probabilidades:", np.sum(probs))
 
