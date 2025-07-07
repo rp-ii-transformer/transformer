c894cccc691ed842e00e6b8a514cdc8e7ad5a8a1
Henrique Almeida <henrique.almeida.dev@gmail.com.>
2025-07-07 10:50:06 -0300

    feat: add cross-entropy loss function and softmax output display.

--- src/transformer/pipeline/softmax.py
+++ src/transformer/pipeline/softmax.py
@@ -1,5 +1,8 @@
 import numpy as np
 
+def cross_entropy_loss(probs, target_idx):
+    return -np.log(probs[target_idx] + 1e-9)  # adiciona epsilon para evitar log(0)
+
 def softmax(x):
     exp_x = np.exp(x - np.max(x, axis=-1, keepdims=True))
     return exp_x / np.sum(exp_x, axis=-1, keepdims=True)
--- src/transformer/pipeline/test_softmax.py
+++ src/transformer/pipeline/test_softmax.py
@@ -1,17 +1,26 @@
 import numpy as np
-from softmax import softmax
+from softmax import softmax, cross_entropy_loss
 
 def main():
-    logits = [1, 2, 3]
-    probs = softmax(logits)
-    idx = np.argmax(probs)
+    logits = [1, 2, 3]              # Saída do modelo (antes do softmax)
+    probs = softmax(logits)         # Converte para probabilidades
+    predicted_idx = np.argmax(probs)
 
+    target_idx = 2  # valor correto índice 2 = (o valor 3)
+
+    loss = cross_entropy_loss(probs, target_idx)
+
+    print()
     print("Logits (pontuações):", ", ".join(str(x) for x in logits))
-    print("Softmax (probabilidades): [", ", ".join(str(p) for p in probs), "]")
+    print("Softmax (probabilidades): [", ", ".join(f"{p}" for p in probs), "]")
     print("Soma das probabilidades:", np.sum(probs))
-    print("A maior probabilidade foi:", np.max(probs))
-    print(f"Logit com maior probabilidade: {logits[idx]}")
-
+    print(f"A maior probabilidade foi: {np.max(probs)}")
+    print(f"Logit com maior probabilidade: {logits[predicted_idx]}")
+    # print(f"Target correto (esperado): {logits[target_idx]}")
+    print(f"Loss (erro): {loss:.4f}")
+    print()
+    print(f"A probabilidade do valor {logits[target_idx]} ser o correto é: {probs[target_idx] * 100:.2f}%")
+    print()
 
 if __name__ == "__main__":
     main()
